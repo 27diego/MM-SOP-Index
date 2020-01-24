776ffe66d448d3b3402ae3f26a6bf6e9f3484ddc
27diego <divegamaravilla@csumb.edu>
2020-01-23 22:52:15 -0800

    finishing add user page

--- sopindex/src/pages/Admin/CreateUser/CreateUser.js
+++ sopindex/src/pages/Admin/CreateUser/CreateUser.js
@@ -1,29 +1,72 @@
 import React, { Component } from "react";
 import "./CreateUser.scss";
+import history from "../../../history";
 
 class CreateUser extends Component {
   state = {
-    accountType: ""
+    accountType: "",
+    userName: "",
+    password: ""
+  };
+
+  selectType = type => {
+    this.setState({ accountType: type });
+  };
+
+  onSubmit = () => {
+    history.push("/Dashboard");
   };
 
   render() {
+    console.log(this.state.accountType);
+
     return (
       <div className="container">
-        <div className="modal">
-          <h2 className="modal__header">Choose Account Type</h2>
-          <div className="modal__options">
-            <span className="modal__options-admin">
+        <div className="menu">
+          <h2 className="menu__header">Choose Account Type</h2>
+          <div className="menu__options">
+            <span
+              onClick={() => this.selectType("menu")}
+              className={`menu__options-menu ${
+                this.state.accountType === "menu" ? "active" : ""
+              } `}
+            >
               <i className="fas fa-user-shield"></i>
             </span>
-            <span className="modal__options-user">
+            <span
+              onClick={() => this.selectType("user")}
+              className={`menu__options-user ${
+                this.state.accountType === "user" ? "active" : ""
+              } `}
+            >
               <i className="fas fa-users"></i>
             </span>
           </div>
-          <div className="modal_">user greedings</div>
-          <div className="modal__form">
-            <input className="modal__form-input" type="text" required />
-            <input className="modal__form-input" type="password" required />
-            <button className="modal__form-submit">Submit</button>
+          <div className="menu__choice-greeting">
+            {this.state.accountType === ""
+              ? "Select type"
+              : this.state.accountType === "menu"
+              ? "Welcome new menu"
+              : "Welcome new user"}
+          </div>
+          <div className="menu__form">
+            {/* <i class="fas fa-at"></i> */}
+            <input
+              className="menu__form-input"
+              placeholder="Username"
+              type="text"
+              required
+            />
+            {/* <i class="fas fa-lock"></i> */}
+            <input
+              className="menu__form-input"
+              placeholder="Password"
+              type="password"
+              required
+            />
+            <button onClick={this.onSubmit} className="menu__form-submit">
+              Submit
+            </button>
           </div>
         </div>
       </div>
